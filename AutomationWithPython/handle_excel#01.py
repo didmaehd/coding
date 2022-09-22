bf40882428bf8213a85ff0ebf1915e54995690ca
didmaehd@gmail.com <didmaehd@gmail.com>
2022-09-22 23:30:35 +0900

    20220922#01

---
+++ AutomationWithPython/handle_excel#01.py
@@ -0,0 +1,13 @@
+import openpyxl as excel # import openpyxl and rename to excel
+book = excel.Workbook() # create workbook
+sheet = book.active # create sheet
+sheet["A1"] = "How are you?" # insert text to A1 on excel 
+book.save("./output/hello.xlsx") # save file
+
+
+sheet["A2"] = "I'm fine, thank you!"
+book.save("./output/hello.xlsx")
+
+
+sheet["A3"] = "Get out of here!"
+book.save("./output/hello.xlsx")
